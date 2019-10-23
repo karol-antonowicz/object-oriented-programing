7ae2e329e8f424dce4f5a9cfd98f7b008918472b
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-10-23 12:51:52 +0200

    added index.html and index.js

--- index.js
+++ index.js
@@ -1,1 +1,104 @@
 console.log('Hello World!')
+
+
+
+/* const student1 = {
+    name: 'Karol',
+    adress: 'Gdansk',
+    studentIdNumber: 144437,
+    profile: 'Math',
+    displayFullInfo: function () {
+
+    },
+    changeId: function (x) {     // substracts studentIdNumber by parameter
+        this.studentIdNumber = this.studentIdNumber - x
+        console.log(this.studentIdNumber)
+    }
+
+}
+
+const student2 = {
+    name: 'Michak',
+    adress: 'Sopot',
+    studentIdNumber: 144387,
+    profile: 'Math',
+    displayFullInfo: function () {   //
+        console.log(`Imie: ${this.name} Adress: ${this.adress} studentIdNumber: ${this.studentIdNumber} Profile: ${this.profile}`)
+    },
+    changeId: function (x) {     // substracts studentIdNumber by parameter
+        this.studentIdNumber = x    // np. nowe id
+        console.log(this.studentIdNumber)
+    }
+}
+*/
+
+var studentsList = [];  // tworzy pustą tablicę gdzie będziemy wrzucać studentów
+
+var Person = {
+    name: '',
+    adress: '',
+    studentIdNumber:'',
+    profile: '',
+    setData: function (name, adres, id, profile) {
+        this.name = name
+        this.adress = adres
+        this.studentIdNumber = id
+        this.profile = profile
+    }
+}
+
+var student1 = Object.create(Person);                   
+student1.setData('Michał', 'Poznań', 14585, 'student')      //tu nalezy podać imię, adres, numer id, profil 
+
+var student2 = Object.create(Person);
+student2.setData('Karol', 'Gdansk', 14586, 'student')
+
+var student3 = Object.create(Person);
+ student3.setData('Łukasz', 'Gdynia', 14587, 'student')
+
+var student4 = Object.create(Person);
+student4.setData('Adam', 'Gdansk', 14588, 'student')
+
+var student5 = Object.create(Person);
+student5.setData('Karol', 'Poznań', 14589, 'student')
+
+var student6 = Object.create(Person);
+student6.setData('Johnny', 'Gdansk', 14590, 'student')
+
+var student7 = Object.create(Person);
+student7.setData('Goro', 'Toruń', 14591, 'student')
+
+var student8 = Object.create(Person);
+student8.setData('Wariacik', 'Gdansk', 14592, 'student')
+
+var student9 = Object.create(Person);
+student9.setData('Siwy', 'Warszawa', 14593, 'student')
+
+var student10 = Object.create(Person);
+student10.setData('Mateusz', 'Gdansk', 14594, 'student')
+
+// dodaje wszystkcih studentów do pustej tablicy studentsList
+
+
+studentsList.push(student1, student2, student3, student4, student5, student6, student7, student8, student9, student10)
+
+
+
+
+var divDisplay = document.querySelector('#display')         // grabs the div on the website
+var output = ''                                            // creates empty output
+
+studentsList.map(x=> output += `<div class="content"> ${x.name} ${x.adress} ${x.studentIdNumber} ${x.profile}</div>`)  
+//mapuje studentlistę i do pustego output wrzuca do niego dla każdego elementu tablicy studentlsita tworzy div o klasie content i wsadza do niego text z nazwa, adresem, id, profilem studenta
+
+divDisplay.innerHTML = output   // dodaje do diva o id display w srodku 
+divDisplay.style.display = 'flex';  // dla diva o id display zmienia display na flex
+divDisplay.style.flexDirection = 'column';  // wiadomo
+divDisplay.style.justifyContent = 'space-around';   // wiadomo
+divDisplay.style.height = '98vh';   // wiadomo
+
+
+
+
+
+
